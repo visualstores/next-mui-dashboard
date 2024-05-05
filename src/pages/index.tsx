de5ec99ad08c27a3443228e8d15aa1fc1c054d56
ojp <ojpfight@gmail.com>
2024-05-05 11:57:23 +0900

    문구 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -14,7 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>mui 테스트</main>
+      <main className={`${styles.main} ${inter.className}`}>
+        mui 배포 테스트
+      </main>
     </>
   )
 }
